2f98aae4064c5dde16f3cfca12cce353f226eebe
1312415 <idkwayta@gmail.com>
2017-05-26 23:43:09 +0700

    update fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 module.exports = function(app){
     app.use(express.static('public'));
+    app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(session( {
         secret : process.env.SESSION_SECRET || 'secret',
