656f730506b65a0bd3f937f43a17081a53b28014
1312415 <idkwayta@gmail.com>
2017-05-26 23:36:09 +0700

    update fix

--- config/passport.js
+++ config/passport.js
@@ -35,7 +35,7 @@ passport.use(new facebookStrategy({
         callbackURL: process.env.FACEBOOKCALLBACK,
         passReqToCallback: true,
         scope: ['user_friends'],
-        profileFields: ['id', 'displayName', 'photos', 'friends']
+        profileFields: ['id', 'displayName', 'photos', 'email', 'friends']
     },
     function (req, accessToken, refreshToken, profile, cb) {
         var email = false;
