2555e2c4166588f663de1d876c3fc9dd8de536fa
1312415 <idkwayta@gmail.com>
2017-05-31 13:19:07 +0700

    force

--- app/db/model.js
+++ app/db/model.js
@@ -7,35 +7,35 @@ const Premium = require('../models/Premium');
 
 
 
-User.sync({force: true})
+User.sync()
     .then(function () {
         Friend.belongsTo(User, {as: "from", foreignKey: "userId"});
         Friend.belongsTo(User, {as: "to", foreignKey: "toUserId"});
         User.hasMany(Friend, {as: "friends", foreignKey: "userId"});
-        return MessageRoom.sync({force: true});
+        return MessageRoom.sync();
     })
     .then(function () {
         Friend.belongsTo(MessageRoom, {as: "messageRoom", foreignKey: "messageRoomId"});
         MessageRoom.hasMany(Friend, {as: "friends", foreignKey: "messageRoomId"});
-        return Friend.sync({force: true});
+        return Friend.sync();
     })
     .then(function () {
         Message.belongsTo(MessageRoom, {as: "messageRoom", foreignKey: "messageRoomId"});
         MessageRoom.hasMany(Message, {as: "messages", foreignKey: "messageRoomId"});
         Message.belongsTo(User,{as: "user", foreignKey: "userId"});
-        Message.sync({force: true});
+        Message.sync();
     })
     .then(function () {
         MessageStatus.belongsTo(User,{as: "user", foreignKey: "userId"});
         User.hasMany(MessageStatus,{as: "messageStatuses", foreignKey: "userId"});
         MessageStatus.belongsTo(User,{as: "from", foreignKey: "fromUserId"});
         MessageStatus.belongsTo(MessageRoom,{as: "in", foreignKey: "inMessageRoomId"});
-        MessageStatus.sync({force: true});
+        MessageStatus.sync();
     })
     .then(function () {
         Premium.belongsTo(User,{as: "user", foreignKey: "userId"});
         User.hasMany(Premium,{as:"premiumNotifications", foreignKey: "userId"});
-        Premium.sync({force: true})
+        Premium.sync()
     })
 ;
 module.exports = {
