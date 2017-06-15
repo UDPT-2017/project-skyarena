c069cb4d1ef86722c61c39758fe8f1e32693d2a0
1312415 <idkwayta@gmail.com>
2017-06-15 11:51:44 +0700

    fix premium

--- app/controllers/postController.js
+++ app/controllers/postController.js
@@ -1,46 +1,48 @@
-const User = require('../db/model').User;
-const Post = require('../db/model').Post;
+const User = require("../db/model").User;
+const Post = require("../db/model").Post;
 
 var postController = {
-    index: function (req, res) {
-        res.render('post', {
-            page: 'post'
-        })
-    },
-    loadPost: function(req, res) {
-        res.render('post/new')
-    },
-    createPost: function(req, res) {
-        var post = Post.build({});
-        post.title = req.body.title;
-        post.content = req.body.content;
-        post.save().then(function(post){
-            req.flash('info', 'Tao bai viet thanh cong');
-            res.redirect('/post/');
-          }).catch(function(e){
-            req.flash('info', 'Nhập dữ liệu sai');
-            res.redirect('/post/new');
-          })
-    },
-    getPost: function (req, res) {
-        var postList = [];
-        var offset = req.query.page ;
-        var query = req.query.query;
-        Post.findAll({
-            where: {
-                postId: req.post.id
-            }
-        }).then(function (posts) {
-            posts.forEach(function (post) {
-              postList.push({
-                  id: post.id,
-                  title: post.title,
-                  content: post.content
-              });
-            });
+  index: function(req, res) {
+    res.render("post", {
+      page: "post"
+    });
+  },
+  loadPost: function(req, res) {
+    res.render("post/new");
+  },
+  createPost: function(req, res) {
+    var post = Post.build({});
+    post.title = req.body.title;
+    post.content = req.body.content;
+    post
+      .save()
+      .then(function(post) {
+        req.flash("info", "Tao bai viet thanh cong");
+        res.redirect("/post/");
+      })
+      .catch(function(e) {
+        req.flash("info", "Nhập dữ liệu sai");
+        res.redirect("/post/new");
+      });
+  },
+  getPost: function(req, res) {
+    var postList = [];
+    var offset = req.query.page;
+    var query = req.query.query;
+    Post.findAll({
+      where: {
+        postId: req.post.id
+      }
+    }).then(function(posts) {
+      posts.forEach(function(post) {
+        postList.push({
+          id: post.id,
+          title: post.title,
+          content: post.content
         });
-    }
-
+      });
+    });
+  }
 };
 
 module.exports = postController;
--- app/controllers/videoController.js
+++ app/controllers/videoController.js
@@ -1,11 +1,14 @@
-
-
 var videoController = {
-    index: function (req, res) {
-        res.render('video', {
-            page: 'video'
-        })
-    }
+  index: function(req, res) {
+    var premium;
+    req.user.premium && req.user.premium >= new Date()
+      ? (premium = true)
+      : (premium = false);
+    res.render("video", {
+      page: "video",
+      premium
+    });
+  }
 };
 
 module.exports = videoController;
