d226a1bbcbb5d0d5c3f025197a9e76e877c10a75
1312415 <idkwayta@gmail.com>
2017-06-18 16:08:49 +0700

    fix bug and Readme

--- seed.js
+++ seed.js
@@ -5,11 +5,11 @@ const Video = require('./app/db/model').Video;
 
 var name = 'phan';
 for(var i = 0; i < 50; i++){
-    // var user = User.build({
-    //     name: name + i.toString(),
-    //     email: name + i.toString() + "@gmail.com",
-    // });
-    // user.save();
+    var user = User.build({
+        name: name + i.toString(),
+        email: name + i.toString() + "@gmail.com",
+    });
+    user.save();
     var video = Video.build({
         title: name + i.toString(),
         description: name + i.toString() + "@gmail.com",
